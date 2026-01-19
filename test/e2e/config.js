936c29a85b3294abbc654ce2231a6cfd8f885e13
John Boxall <bishanty@gmail.com>
2026-01-19 13:13:11 -0800

    test config

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  hostname: process.env.SFCC_HOSTNAME || 'blwf-001.dx.commercecloud.salesforce.com',
-  siteId: process.env.SFCC_SITE_ID || 'RefArch',
+  hostname: process.env.B2C_HOSTNAME || 'blwf-001.dx.commercecloud.salesforce.com',
+  siteId: process.env.B2C_SITE_ID || 'RefArch',
   testLandingSlug: process.env.TEST_LANDING_SLUG || 'statement-mini-dresses-for-parties-a56e63f6',
 };
 
