3e25863585924fe07c2679c5a0cc933bcffa72b8
John Boxall <bishanty@gmail.com>
2026-01-19 13:04:15 -0800

    basic testing

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    files: ['cartridges/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+      globals: {
+        // SFCC globals
+        dw: 'readonly',
+        request: 'readonly',
+        response: 'readonly',
+        session: 'readonly',
+        customer: 'readonly',
+        // Node globals
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'object-shorthand': 'error',
+      'no-unneeded-ternary': 'error',
+      'prefer-template': 'error',
+      'no-empty': ['error', { allowEmptyCatch: true }],
+    },
+  },
+  eslintConfigPrettier,
+];
